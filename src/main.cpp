27e91102982543abe2d258da8edeb266e4897d8e
vignesh <vigneshs@melss.com>
2023-10-05 18:27:46 +0530

    MQTT_OOP_Starting

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 // Your MQTT broker ID
 const char *mqttBroker = "broker.hivemq.com";
-const int mqttPort = 1883;
+int mqttPort = 1883;
 // MQTT topics
 const char *publishTopic = "halleffect";
 const char *subscribeTopic = "led_oop";
@@ -29,6 +29,84 @@ const char* password = "vicky12345";
 Led led(LED_PIN);
 Button button(BUTTON_PIN);
 WiFiAP1 wifiAP1(ssid, password);
+
+class MQTT1
+{
+  private:
+    const char *mqttBroker;
+    int mqttPort;
+    const char *publishTopic;
+    const char *subscribeTopic;
+
+  public:
+    MQTT1() {}
+
+    void init(const char *mqttBroker, int mqttPort)
+    {
+      this->mqttBroker = mqttBroker;
+      this->mqttPort = mqttPort;
+      // setup the mqtt server and callback
+      client.setServer(mqttBroker, mqttPort);
+      // client.setCallback(callback);
+    }
+
+    // Callback function whenever an MQTT message is received
+    void callback(char *topic, byte *payload, unsigned int length)
+    {
+      this->subscribeTopic = topic;
+
+      Serial.print("Message arrived [");
+      Serial.print(topic);
+      Serial.print("] ");
+      String message;
+      for (int i = 0; i < length; i++)
+      {
+        Serial.print(message += (char)payload[i]);
+      }
+      Serial.println();
+
+      // Switch on the LED if 'ON' was received
+      if (message == "ON")
+      {
+        Serial.println("Turning ON Built In LED..");
+        // led.on();
+      }
+      else
+      {
+        Serial.println("Turning OFF Built In LED..");
+        // led.off();
+      }
+    }
+
+    void reconnect()
+    {
+      // Loop until we're reconnected
+      while (!client.connected())
+      {
+        Serial.print("Attempting MQTT connection...");
+
+        // Create a random client ID
+        String clientId = "ESP32Client-";
+        clientId += String(random(0xffff), HEX);
+
+        // Attempt to connect
+        if (client.connect(clientId.c_str()))
+        {
+          Serial.println("connected");
+          // Subscribe to topic
+          client.subscribe(subscribeTopic);
+        }
+        else
+        {
+          Serial.print("failed, rc=");
+          Serial.print(client.state());
+          Serial.println(" try again in 5 seconds");
+          // Wait 5 seconds before retrying
+          delay(5000);
+        }
+      }
+
+};
 
 // Callback function whenever an MQTT message is received
 void callback(char *topic, byte *payload, unsigned int length)
