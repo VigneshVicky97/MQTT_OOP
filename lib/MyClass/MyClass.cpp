eeee4681118e881b91bb16591004f05f6ac78ecc
vignesh <vigneshs@melss.com>
2023-10-06 12:14:46 +0530

    Created MyClass for MQTT using MQTTClient

---
+++ lib/MyClass/MyClass.cpp
@@ -0,0 +1,20 @@
+#include "MyClass.h"
+
+
+MyClass::MyClass(void) 
+{
+};
+
+void MyClass::loop(void)
+{
+  currentMillis = millis();
+  if( myClass_mqttClient->connected() && (currentMillis - lastPublishMillis > 10000)) 
+  {
+    lastPublishMillis = currentMillis;
+
+    myClass_mqttClient->publish(myClass_topic,"Message from MyClass loop");
+    Serial.printf("Message from MyClass loop published to topic %s\n", myClass_topic);
+  }
+
+};
+
---
+++ lib/MyClass/MyClass.h
@@ -0,0 +1,51 @@
+#ifndef MyClass_H
+#define MyClass_H
+
+#include <MQTTClient.h>
+
+#include <string.h>
+
+class MyClass: public MQTTClientCallback {
+
+private:
+
+  MQTTClient * myClass_mqttClient;
+
+  unsigned long currentMillis = millis();
+  unsigned long lastPublishMillis = currentMillis;
+
+  const char* myClass_topic = "/topic/example/myClass";
+  const char* myClass_Sub_topic = "/iot-button/topic1";
+
+  // MQTTClient Observer callback functions:
+  virtual void onConnected(MQTTClient* client) 
+  { 
+    Serial.println("MyClass receives MQTTClient onConnected callback. The MQTT client is connected to the broker now.");
+  };
+	virtual void onDataReceived(MQTTClient* client, const mqtt_client_event_data *data) 
+  { 
+    Serial.printf("[%lu] +++ MyClass receives MQTT message of size %d on topic %s: %.*s\r\n", millis(), data->data_len, data->topic.c_str(), data->data_len, data->data);
+  };
+  virtual void onSubscribed(MQTTClient* thisClient, const mqtt_client_topic_data *topic)
+  { 
+    Serial.printf("MyClass receives new topic subscription status -> Topic[%d]: %s status %d\n", topic->subs_msg_id, topic->topic.c_str(), topic->subs_status);
+  };
+
+
+public:
+
+  MyClass(void);
+  void loop(void);
+
+  // MQTTClient Observer callback functions:
+  void setMQTTClient(MQTTClient *client)
+  {
+    // Set MyClass MQTT client and its observer callbacks:
+    this->myClass_mqttClient = client;
+    this->myClass_mqttClient->addCallback(this);
+    // Subscribe to topic example to receive published messages
+    this->myClass_mqttClient->addTopicSub(myClass_Sub_topic);
+  }
+
+};
+#endif
